b6dfa044a69605202caadf888659d98272fc9bff
Faissal Elamraoui <amr.faissal@gmail.com>
2018-10-24 21:45:05 +0200

    Release v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## v0.2.0
+
+- Support for Elixir 1.6
+- Ability to unmarshal NDJSON from a file
+- Fixes & Improvements
+
 ## v0.1.0
 
 - Basic functionality
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExNdjson.MixProject do
   def project do
     [
       app: :ex_ndjson,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       preferred_cli_env: [
